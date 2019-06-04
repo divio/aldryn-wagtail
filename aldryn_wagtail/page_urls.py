c669fa68d4d0d99aae517a3c19af71bec887463e
Angelo Dini <angelo.dini@divio.ch>
2019-06-04 11:55:42 +0200

    run isort

--- aldryn_wagtail/page_urls.py
+++ aldryn_wagtail/page_urls.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 
 from wagtail.core import urls as wagtail_urls
+
 
 urlpatterns = [
     # For anything not caught by a more specific rule, hand over to
--- aldryn_wagtail/urls.py
+++ aldryn_wagtail/urls.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
-from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.core import urls as wagtail_urls
+from wagtail.documents import urls as wagtaildocs_urls
 
 
 urlpatterns = [
