605ff17415af90164b1c712a073b80cf92071d14
Angelo Dini <angelo.dini@divio.ch>
2023-06-28 09:15:18 +0200

    bumped version to 5.0.2.0

--- aldryn_wagtail/__init__.py
+++ aldryn_wagtail/__init__.py
@@ -1,2 +1,2 @@
 # -*- coding: utf-8 -*-
-__version__ = '5.0.1.0'
+__version__ = '5.0.2.0'
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from aldryn_wagtail import __version__
 
 REQUIREMENTS = [
     'aldryn-addons',
-    'wagtail==5.0.1',
+    'wagtail==5.0.2',
 ]
 
 
